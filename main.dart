791d3244802359f86d1c7829987534adef3d5770
kemillyy <126573935+kemillyy@users.noreply.github.com>
2025-10-30 00:16:42 +0000

    featexercicios

--- main.dart
+++ main.dart
@@ -1,20 +1,153 @@
-import 'dart:io';
+    import 'package:flutter/material.dart';
 
-void main(){
+void main() => runApp(
+      MaterialApp(
+        home: Home(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
 
-    List<String?> alunos = [];
-    bool cadastrarOutroAluno = true;
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
 
-    while(cadastrarOutroAluno){
-        print("Informe nome do aluno:");
-       din.readLineSyn String? nome = stc();
+class _HomeState extends State<Home> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-        alunos.add(nome);
+  final TextEditingController _weightController = TextEditingController();
+  final TextEditingController _heightController = TextEditingController();
+  String _result = 'Informe seus dados';
 
-        print('Deseja cadastrar outro aluno?(s/n)');
-        var repostaUsuario = stdin.readLineSync();
-        cadastrarOutroAluno = repostaUsuario == "s";
-        
-    };
+  @override
+  void initState() {
+    super.initState();
+    resetFields();
+  }
 
+  void resetFields() {
+    _weightController.text = '';
+    _heightController.text = '';
+    setState(() {
+      _result = 'Informe seus dados';
+    });
+  }
+
+  void calculateImc() {
+    double weight = double.tryParse(_weightController.text) ?? 0.0;
+    double height = (double.tryParse(_heightController.text) ?? 0.0) / 100.0;
+    if (height <= 0 || weight <= 0) {
+      setState(() {
+        _result = "Dados inválidos";
+      });
+      return;
+    }
+
+    double imc = weight / (height * height);
+
+    setState(() {
+      _result = "IMC = ${imc.toStringAsPrecision(4)}\n";
+      if (imc < 18.6)
+        _result += "Abaixo do peso";
+      else if (imc < 25.0)
+        _result += "Peso ideal";
+      else if (imc < 30.0)
+        _result += "Levemente acima do peso";
+      else if (imc < 35.0)
+        _result += "Obesidade Grau I";
+      else if (imc < 40.0)
+        _result += "Obesidade Grau II";
+      else
+        _result += "Obesidade Grau III";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20.0),
+        child: buildForm(),
+      ),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      title: const Text('Calculadora de IMC'),
+      backgroundColor: Colors.blue,
+      actions: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.refresh),
+          onPressed: resetFields,
+        )
+      ],
+    );
+  }
+
+  Form buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          buildTextFormField(
+            label: "Peso (kg)",
+            error: "Insira seu peso!",
+            controller: _weightController,
+          ),
+          buildTextFormField(
+            label: "Altura (cm)",
+            error: "Insira uma altura!",
+            controller: _heightController,
+          ),
+          buildTextResult(),
+          buildCalculateButton(),
+        ],
+      ),
+    );
+  }
+
+  Padding buildCalculateButton() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 36.0),
+      child: ElevatedButton(
+        onPressed: () {
+          if (_formKey.currentState?.validate() ?? false) {
+            calculateImc();
+          }
+        },
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+        child: const Text('CALCULAR', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+
+  Padding buildTextResult() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 36.0),
+      child: Text(
+        _result,
+        textAlign: TextAlign.center,
+        style: const TextStyle(fontSize: 18),
+      ),
+    );
+  }
+
+  TextFormField buildTextFormField({
+    required TextEditingController controller,
+    required String error,
+    required String label,
+  }) {
+    return TextFormField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(labelText: label),
+      controller: controller,
+      validator: (text) {
+        return (text == null || text.isEmpty) ? error : null;
+      },
+    );
+  }
 }
